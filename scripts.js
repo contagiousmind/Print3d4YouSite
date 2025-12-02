a944a63589328b10dd0a2bf849b91d77e977a5e4
contagiousmind <sebmann@gmail.com>
2025-12-02 21:38:17 +0000

    initial content

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-sssd
+
+$(function () {
+    
+    // always start by loading home
+    MenuItem_Click('Home');
+
+});
+
+
+function MenuItem_Click(tab) {
+
+    // remove selecteds
+    $(".mainitemouter.selected").removeClass('selected');
+
+    // and add to one we're selecting
+    $("#MenuItemOuter_" + tab).addClass("selected");
+
+    // hide other tabs...
+    $(".maincontent").hide(); 
+
+    // and show tab...
+    $("#MainContent_" + tab).show();
+
+}
+
+
+
+
